b7cb661d1f5508edb1ede025e06f88581226f11e
Tammyhi <85895125+Tammyhi@users.noreply.github.com>
2024-06-28 15:14:57 -0400

    Preserve links through localStorage

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ let myLinks = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let pastLinks = JSON.parse(localStorage.getItem("myLinks"))
+if(pastLinks != null){
+    myLinks = pastLinks
+    renderLinks()
+}
+console.log(pastLinks)
+
 
 inputBtn.addEventListener("click",function(){ /*What happens on click of Input button*/
     myLinks.push(inputEl.value)
-    renderLinks()
     inputEl.value = ""
+    localStorage.setItem("myLinks",JSON.stringify(myLinks))
+    renderLinks()
+    console.log(localStorage.getItem("myLinks"))
 })
 
 function renderLinks(){
