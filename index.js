704c4b22b31eaeb1e204857d60821f896104dbbe
Tammyhi <85895125+Tammyhi@users.noreply.github.com>
2024-06-28 15:32:16 -0400

    Add delete button functionality

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ let myLinks = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-let pastLinks = JSON.parse(localStorage.getItem("myLinks"))
-if(pastLinks != null){
+const deleteBtn = document.getElementById("delete-btn")
+const pastLinks = JSON.parse(localStorage.getItem("myLinks"))
+if(pastLinks){
     myLinks = pastLinks
-    renderLinks()
+    render(myLinks)
 }
 console.log(pastLinks)
 
@@ -14,17 +15,23 @@ inputBtn.addEventListener("click",function(){ /*What happens on click of Input b
     myLinks.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLinks",JSON.stringify(myLinks))
-    renderLinks()
+    render(myLinks)
     console.log(localStorage.getItem("myLinks"))
 })
 
-function renderLinks(){
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLinks = []
+    render(myLinks)
+})
+
+function render(links){
     let listItems = ""
-    for(let i = 0; i < myLinks.length; i++){
+    for(let i = 0; i < links.length; i++){
         listItems += `
             <li>
-                <a target='_blank' href='${myLinks[i]}'>
-                    ${myLinks[i]}
+                <a target='_blank' href='${links[i]}'>
+                    ${links[i]}
                 </a>
         `
     }
